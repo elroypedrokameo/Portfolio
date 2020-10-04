1322ecb8237e635f554b9b1d7adf2f95d657936c
Elroy Pedro Kameo <infinitygamexray@gmail.com>
2020-10-05 05:31:20 +0700

    Menambahkan Sweetalert

--- js/main.js
+++ js/main.js
@@ -1,34 +1,11 @@
-$darkMode = false
-
-if (localStorage.getItem('defaultTheme') == 'dark') {
-    toggleDarkMode()
-}
-
-function toggleDarkMode() {
-    if ($darkMode == false) {
-        document.body.style.backgroundColor = "rgb(85, 87, 86)"
-        document.body.style.color = "White"
-        document.getElementById("themeInfo").innerHTML = "Dark!"
-        localStorage.setItems('defaultTheme', 'dark');
-        $darkMode = true
-    } else {
-        document.body.style.backgroundColor = "White"
-        document.body.style.color = "Black"
-        document.getElementById("themeInfo").innerHTML = "Light!"
-        localStorage.setItems('defaultTheme');
-        $darkMode = false
-    }
-}
-
-
 function validasi() {
     var nama = document.getElementById("nama").value;
     var email = document.getElementById("email").value;
     var telp = document.getElementById("telp").value;
 
     if (nama != "" && email != "" && telp != "") {
-        return true;
+        swal("Succes!", "Terimakasih telah menghubungi kami", "success");
     } else {
-        alert('Data yang anda masukan kurang lengkap!')
+        swal("Error", "Maaf Data Yang Anda Inputkan Tidak Lengkap!", "error");
     }
 }
